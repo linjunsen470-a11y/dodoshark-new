6f14a17d79df4bab492599fa788224095e8d24a3
linjunsen <linjunsen470@gmail.com>
2026-03-30 13:20:01 +0800

    3.30 1pm 2nd

--- front/dodoshark/app/api/draft/enable/route.ts
+++ front/dodoshark/app/api/draft/enable/route.ts
@@ -1,11 +1,22 @@
 import {defineEnableDraftMode} from 'next-sanity/draft-mode'
 
 import {client} from '@/app/lib/sanity'
-import {getSanityReadToken} from '@/app/lib/sanity.token'
+import {sanityReadToken} from '@/app/lib/sanity.token'
 
-export const {GET} = defineEnableDraftMode({
-  client: client.withConfig({
-    token: getSanityReadToken(),
-    useCdn: false,
-  }),
-})
+export const GET = async (request: Request) => {
+  if (!sanityReadToken) {
+    return new Response(
+      'Visual editing is disabled: Missing SANITY_API_READ_TOKEN environment variable in production.',
+      {status: 401}
+    )
+  }
+
+  const {GET: enableDraftMode} = defineEnableDraftMode({
+    client: client.withConfig({
+      token: sanityReadToken,
+      useCdn: false,
+    }),
+  })
+
+  return enableDraftMode(request)
+}
