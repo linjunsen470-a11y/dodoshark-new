49d22eaad67040458657ec48e56dc9c50fbdad41
linjunsen <linjunsen470@gmail.com>
2026-04-01 13:01:57 +0800

    4.1 clear root direcotry unused files

--- fix.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs')
-const path = 'd:/new-website/final-3.9/front/dodoshark/app/products/[slug]/page.tsx'
-let text = fs.readFileSync(path, 'utf8')
-text = text.replace(/`r`n/g, '')
-fs.writeFileSync(path, text)
